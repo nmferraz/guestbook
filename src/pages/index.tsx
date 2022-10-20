f71989d2b50323aa070e86139b99830b93d990bd
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-10-20 10:15:15 +0100

    component to display the messages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,24 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import { trpc } from "../utils/trpc";
+
+const Messages = () => {
+  const { data: messages, isLoading } = trpc.useQuery(["guestbook.getAll"]);
+
+  if (isLoading) return <div>Fetching messages...</div>;
+
+  return (
+    <div className="flex flex-col gap-4">
+      {messages?.map((msg, index) => {
+        return (
+          <div key={index}>
+            <p>{msg.message}</p>
+            <span>- {msg.name}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 const Home = () => {
   const { data: session, status } = useSession();
