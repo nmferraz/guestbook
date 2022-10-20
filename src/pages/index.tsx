05b3777564c24b02aef0d7651e24cc740edabfb2
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-10-20 10:11:53 +0100

    some styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,31 @@ const Home = () => {
   const { data: session, status } = useSession();
 
   if (status === "loading") {
-    return <main>Loading...</main>;
+    return <main className="flex flex-col items-center pt-4">Loading...</main>;
   }
 
   return (
-    <main>
-      <h1>Guestbook</h1>
+    <main className="flex flex-col items-center">
+      <h1 className="text-3xl pt-4">Guestbook</h1>
+      <p>
+        Tutorial for <code>create-t3-app</code>
+      </p>
 
-      {session ? (
-        <div>
-          <p>hi {session.user?.name}</p>
+      <div className="pt-10">
+        {session ? (
+          <div>
+            <p>hi {session.user?.name}</p>
 
-          <button onClick={() => signOut()}>Logout</button>
-        </div>
-      ) : (
-        <div>
-          <button onClick={() => signIn("discord")}>Login with Discord</button>
-        </div>
-      )}
+            <button onClick={() => signOut()}>Logout</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={() => signIn("discord")}>
+              Login with Discord
+            </button>
+          </div>
+        )}
+      </div>
     </main>
   );
 };
