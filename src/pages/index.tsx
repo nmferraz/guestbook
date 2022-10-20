d046790f5ff28d8232524b7317d541977c1a226d
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-10-20 09:45:43 +0100

    useSession() hook + signOut() function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <main>Loading...</main>;
+  }
+
   return (
     <main>
       <h1>Guestbook</h1>
 
-      <button onClick={() => signIn("discord")}>Login with Discord</button>
+      {session ? (
+        <div>
+          <p>hi {session.user?.name}</p>
+
+          <button onClick={() => signOut()}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => signIn("discord")}>Login with Discord</button>
+        </div>
+      )}
     </main>
   );
 };
