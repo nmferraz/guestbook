70cae39142d2d85df5c1f2962d91db663ccaa789
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-10-21 09:36:40 +0100

    try again

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
-import { env } from "./src/env/server.mjs";
-
-/**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
- * @template {import('next').NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
- */
-function defineNextConfig(config) {
-  return config;
-}
+/** @type {import('next').NextConfig} */
 
 module.exports = {
   i18n: {
@@ -33,8 +21,19 @@ module.exports = {
   }
 }
 
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config;
+}
+
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
-  i18n: true,
 });
