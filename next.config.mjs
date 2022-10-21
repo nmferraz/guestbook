ede6ac8a7f2e11950d82ca4d1728555326ae6398
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-10-21 09:39:45 +0100

    try to solve Build error occurred

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
-/** @type {import('next').NextConfig} */
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config;
+}
 
+export default defineNextConfig({
+  reactStrictMode: true,
+  swcMinify: true,
+});
+
+/** @type {import('next').NextConfig} */
 module.exports = {
   i18n: {
     locales: ["en", "pt", "es"],
@@ -20,20 +36,3 @@ module.exports = {
     ]
   }
 }
-
-/**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
- * @template {import('next').NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
- */
-function defineNextConfig(config) {
-  return config;
-}
-
-export default defineNextConfig({
-  reactStrictMode: true,
-  swcMinify: true,
-});
