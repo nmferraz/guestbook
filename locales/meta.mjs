6326f47b472bb712179c5f17ac1977a6489fae9e
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-10-22 09:23:26 +0100

    meta locales

---
+++ locales/meta.mjs
@@ -0,0 +1,14 @@
+export default {
+    'pt': {
+        flag: '',
+        label: 'Português'
+    },
+    'en': {
+        flag: '',
+        label: 'English'
+    },
+    'es': {
+        flag: '',
+        label: 'Spanish'
+    }
+}
