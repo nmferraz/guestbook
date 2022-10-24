7abd5f03fc79b8ebe5784318aca17be3a744d9ab
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-10-24 10:03:44 +0100

    pt

--- locales/texts.mjs
+++ locales/texts.mjs
@@ -1,6 +1,13 @@
 export default {
     'pt': {
-      title: 'Deixa a tua mensagem, para que o mundo a possa ler',
+    title: 'Deixa a tua mensagem, para que o mundo a possa ler',
+    fetching: 'A carregar as mensagens...',
+    loading: 'A carregar...',
+    hi: 'Olá,',
+    logout: 'Sair da conta',
+    yourmessage: 'A tua mensagem...',
+    submit: ' Enviar',
+    discord: 'Iniciar Sessão com o Discord',
     },
     'en': {
       title: 'Leave your message for the world could see it!',
